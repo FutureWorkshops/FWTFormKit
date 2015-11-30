f3e49e91a1c45fdd4f9013bb99db5ef01b829c2f
Eugene Prokoshev <yevgeniy@futureworkshops.com>
2015-11-30 16:05:24 +0000

    Refactored TextFieldInputdelegate
    Signed-off-by: Eugene Prokoshev <yevgeniy@futureworkshops.com>

--- FormsManager/Cells/Protocols/FWTTextFieldInputDelegate.h
+++ FormsManager/Cells/Protocols/FWTTextFieldInputDelegate.h
@@ -10,7 +10,9 @@
 
 @protocol FWTTextFieldInputDelegate <NSObject>
 
--(void) fwt_textFieldBeginEditing:(UITextField *) textField;
--(void) fwt_textFieldEndEditing:(UITextField *) textField;
+-(void) cell:(id) cell textFieldBeginEditing:(UITextField *) textField;
+
+@optional
+-(void) cell:(id) cell textFieldEndEditing:(UITextField *) textField;
 
 @end
